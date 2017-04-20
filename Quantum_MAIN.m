c7d9791163945a98bd7d37e282e09d2c9571b3f7
Marcel Nunez <mpnunez@udel.edu>
2017-04-19 21:43:15 -0400

    Started making a GUI

--- Quantum_MAIN.m
+++ Quantum_MAIN.m
@@ -1,5 +1,5 @@
-function Quantum_MAIN
-
+function output = Quantum_MAIN
+gui
 format long;
 clear;
 clc;
@@ -17,18 +17,19 @@ m = 1.62661e-27;                            % Kg
 
 t = cputime;
 
-N = 16;                                     % N + 1 basis functions are used, MUST BE EVEN
-kay = 6;                                    % number of energy levels retrieved with Lanczos
-M = 2 * N + 1;
+n_bais_vecs = 14;                                     % N + 1 basis functions are used
+n_elect = 5;        % number of electrons in the system
+n_eng_levels = ceil(n_elect / 2);                                    % number of energy levels retrieved with Lanczos
+M = 2 * n_bais_vecs + 1;
 x = linspace(-L/2,L/2 - L/M,M);
 V = zeros(1,M);
 
 for i = 1:M
-    V(i) = potential(x(i));
+    V(i) = harm_pot(x(i));
 end
 
 freq = fft(V);                     % Take fft of the potential
-freq = circshift(freq,[1,N]);
+freq = circshift(freq,[1,n_bais_vecs]);
 
 %Look at the potential and it's Fourier transform
 
@@ -47,21 +48,21 @@ freq = circshift(freq,[1,N]);
 
 %% Build Hamiltonian Matrix
 
-PW = -N/2:N/2;
+PW = -n_bais_vecs/2 : n_bais_vecs/2;
 
 % Build kinetic energy matrix
-Ham_KE = zeros(N+1,N+1);
-for k = 1:N+1
+Ham_KE = zeros(n_bais_vecs+1,n_bais_vecs+1);
+for k = 1:n_bais_vecs+1
     Ham_KE(k,k) = hbar ^ 2 / 2 / m * L ^ -2 * 4 * pi^2 * PW(k)^2;
 end
 
 % Build potential energy matrix
-Ham_PE = zeros(N+1,N+1);
+Ham_PE = zeros(n_bais_vecs+1,n_bais_vecs+1);
 
-for i = 1:N+1
-    for j = 1:N+1
+for i = 1:n_bais_vecs+1
+    for j = 1:n_bais_vecs+1
         freqdiff = PW(i) - PW(j);
-        index = freqdiff + N + 1;
+        index = freqdiff + n_bais_vecs + 1;
         Ham_PE(i,j) = freq(index)/M;
     end
 end
@@ -69,22 +70,24 @@ end
 Ham = Ham_KE + Ham_PE;
 Ham = real(Ham);
 
-FFTtime = cputime - t
+FFTtime = cputime - t;
 
 %% Solve SE
 
 t = cputime;
 
-[Vecs, Vals] = eig(Ham);
-%[Vecs, Vals] = eigs(Ham,kay,'SM');
+[Vecs, Vals] = eig(Ham);          % returns all eigenvalues and
+% eigenvectors
+% [Vecs, Vals] = eigs(Ham,n_eng_levels,'SM');       % This does not work...
 %Vec = real(Vecs);
 %Vals = real(Vals);
 
-Eigtime = cputime - t
+%Eigtime = cputime - t
+
 %% Plot Electron Densities
 % and compare to analytical solution
 
-el = 0;                                 % energy level, only ground state wavefunction can be computed anyway
+eng_lev = 0;                                 % energy level, only ground state wavefunction can be computed anyway
 s = 100;                                % sample for graphing
 xvec = linspace(-L/2,L/2,s);
 xscale = sqrt(hbar/m/omega);            % m
@@ -93,19 +96,19 @@ xscale = sqrt(hbar/m/omega);            % m
 analwf = zeros(1,s);
 analpd = zeros(1,s);
 for i = 1:s
-    analwf(i) = solwf(el,xvec(i));
+    analwf(i) = solwf(eng_lev,xvec(i));
     analpd(i) = abs(analwf(i))^2;
 end
 
 % Approximate Solution
-phimat = zeros(s,N+1);
+phimat = zeros(s,n_bais_vecs+1);
 for i = 1:s
-    for j = 1:N+1
+    for j = 1:n_bais_vecs+1
         phimat(i,j) = phi(PW(j),xvec(i));
     end
 end
 
-wfsln = Vecs(:,el+1);
+wfsln = Vecs(:,eng_lev+1);
 approxwf = phimat*wfsln;
 approxpd = zeros(1,s);
 for i = 1:s
@@ -127,10 +130,10 @@ end
 
 % Compare Energies
 E_approx = diag(Vals)';
-Eng_lvl = 0:N;
-E_anal = zeros(1,N+1);
+Eng_lvl = 0:n_bais_vecs;
+E_anal = zeros(1,n_bais_vecs+1);
 
-for i = 1:N+1
+for i = 1:n_bais_vecs+1
     E_anal(i) = Eng(i-1);
 end
 
@@ -143,46 +146,50 @@ E_approx = E_approx / engscale;
 set(0,'defaultlinelinewidth',1.5)
 set(0,'defaultaxeslinewidth',2)
 
-figure
-plot(PW, real(wfsln), 'o')
-xlabel('Quantum # of PW')
-ylabel('Real Eigenvalue Coeff.')
-ax = gca;
-ax.FontSize = 20;
+% figure
+% plot(PW, real(wfsln), 'o')
+% xlabel('Quantum # of PW')
+% ylabel('Real Eigenvalue Coeff.')
+% ax = gca;
+% ax.FontSize = 20;
+% 
+% figure
+% plot(PW, imag(wfsln), 'o')
+% xlabel('Quantum # of PW')
+% ylabel('Im Eigenvalue Coeff.')
+% ax = gca;
+% ax.FontSize = 20;
+% 
+% figure
+% plot(xvec, real(analwf), xvec, real(approxwf))
+% xlabel('Dimensionles Position')
+% ylabel('Real Dimensionless WF')
+% legend('Analytical', 'Numerical')
+% legend('boxoff')
+% ax = gca;
+% ax.FontSize = 20;
+% 
+% figure
+% plot(xvec, imag(analwf), xvec, imag(approxwf))
+% xlabel('Dimensionles Position')
+% ylabel('Im Dimensionless WF')
+% legend('Analytical', 'Numerical')
+% legend('boxoff')
+% ax = gca;
+% ax.FontSize = 20;
 
-figure
-plot(PW, imag(wfsln), 'o')
-xlabel('Quantum # of PW')
-ylabel('Im Eigenvalue Coeff.')
-ax = gca;
-ax.FontSize = 20;
+% figure
+% plot(xvec,analpd,xvec,approxpd)
+% xlabel('Dimensionles Position')
+% ylabel('Dimensionless PDF')
+% legend('Analytical', 'Numerical')
+% legend('boxoff')
+% ax = gca;
+% ax.FontSize = 20;
 
-figure
-plot(xvec, real(analwf), xvec, real(approxwf))
-xlabel('Dimensionles Position')
-ylabel('Real Dimensionless WF')
-legend('Analytical', 'Numerical')
-legend('boxoff')
-ax = gca;
-ax.FontSize = 20;
-
-figure
-plot(xvec, imag(analwf), xvec, imag(approxwf))
-xlabel('Dimensionles Position')
-ylabel('Im Dimensionless WF')
-legend('Analytical', 'Numerical')
-legend('boxoff')
-ax = gca;
-ax.FontSize = 20;
-
-figure
-plot(xvec,analpd,xvec,approxpd)
-xlabel('Dimensionles Position')
-ylabel('Dimensionless PDF')
-legend('Analytical', 'Numerical')
-legend('boxoff')
-ax = gca;
-ax.FontSize = 20;
+output.a = xvec;
+output.b = analpd;
+output.c = approxpd;
 
 % figure
 % plot(xvec,real(approxwf))
@@ -206,9 +213,11 @@ ax.FontSize = 20;
 
 end
 
-%% Auxiliary functions
 
-function V = potential(x)
+
+%% Harmonic potential
+
+function V = harm_pot(x)
 
 global omega L m
 
@@ -216,6 +225,8 @@ global omega L m
 V = 0.5 * m * omega^2 * x ^2;
 
 end
+
+%% Plane wave
 
 function wf = phi(n,x)
 
@@ -227,7 +238,8 @@ wf = 1/sqrt(L) * exp( 2 * pi * 1i * n * xp);
 
 end
 
-% Analytical Wavefunction Solution
+%% Analytical Wavefunction Solution
+% n - energy level, starting at 0
 function wf = solwf(n,x)
 
 global L hbar omega m
@@ -239,7 +251,8 @@ wf = (alpha / pi)^0.25 / sqrt(2^n * factorial(n)) * hermite(n,y) * exp(-y^2/2);
 
 end
 
-% Analitical Energy
+%% Analitical Energy
+% n - energy level, starting at 0
 function E = Eng(n)
 
 global hbar omega
---
+++ gui.m
@@ -0,0 +1,206 @@
+function varargout = gui(varargin)
+% GUI MATLAB code for gui.fig
+%      GUI, by itself, creates a new GUI or raises the existing
+%      singleton*.
+%
+%      H = GUI returns the handle to a new GUI or the handle to
+%      the existing singleton*.
+%
+%      GUI('CALLBACK',hObject,eventData,handles,...) calls the local
+%      function named CALLBACK in GUI.M with the given input arguments.
+%
+%      GUI('Property','Value',...) creates a new GUI or raises the
+%      existing singleton*.  Starting from the left, property value pairs are
+%      applied to the GUI before gui_OpeningFcn gets called.  An
+%      unrecognized property name or invalid value makes property application
+%      stop.  All inputs are passed to gui_OpeningFcn via varargin.
+%
+%      *See GUI Options on GUIDE's Tools menu.  Choose "GUI allows only one
+%      instance to run (singleton)".
+%
+% See also: GUIDE, GUIDATA, GUIHANDLES
+
+% Edit the above text to modify the response to help gui
+
+% Last Modified by GUIDE v2.5 19-Apr-2017 21:27:54
+
+% Begin initialization code - DO NOT EDIT
+gui_Singleton = 1;
+gui_State = struct('gui_Name',       mfilename, ...
+                   'gui_Singleton',  gui_Singleton, ...
+                   'gui_OpeningFcn', @gui_OpeningFcn, ...
+                   'gui_OutputFcn',  @gui_OutputFcn, ...
+                   'gui_LayoutFcn',  [] , ...
+                   'gui_Callback',   []);
+if nargin && ischar(varargin{1})
+    gui_State.gui_Callback = str2func(varargin{1});
+end
+
+if nargout
+    [varargout{1:nargout}] = gui_mainfcn(gui_State, varargin{:});
+else
+    gui_mainfcn(gui_State, varargin{:});
+end
+% End initialization code - DO NOT EDIT
+
+
+% --- Executes just before gui is made visible.
+function gui_OpeningFcn(hObject, eventdata, handles, varargin)
+% This function has no output args, see OutputFcn.
+% hObject    handle to figure
+% eventdata  reserved - to be defined in a future version of MATLAB
+% handles    structure with handles and user data (see GUIDATA)
+% varargin   command line arguments to gui (see VARARGIN)
+
+% Choose default command line output for gui
+handles.output = hObject;
+
+% Update handles structure
+guidata(hObject, handles);
+
+% UIWAIT makes gui wait for user response (see UIRESUME)
+% uiwait(handles.figure1);
+
+
+% --- Outputs from this function are returned to the command line.
+function varargout = gui_OutputFcn(hObject, eventdata, handles) 
+% varargout  cell array for returning output args (see VARARGOUT);
+% hObject    handle to figure
+% eventdata  reserved - to be defined in a future version of MATLAB
+% handles    structure with handles and user data (see GUIDATA)
+
+% Get default command line output from handles structure
+varargout{1} = handles.output;
+
+
+
+function edit1_Callback(hObject, eventdata, handles)
+% hObject    handle to edit1 (see GCBO)
+% eventdata  reserved - to be defined in a future version of MATLAB
+% handles    structure with handles and user data (see GUIDATA)
+
+% Hints: get(hObject,'String') returns contents of edit1 as text
+%        str2double(get(hObject,'String')) returns contents of edit1 as a double
+
+
+% --- Executes during object creation, after setting all properties.
+function edit1_CreateFcn(hObject, eventdata, handles)
+% hObject    handle to edit1 (see GCBO)
+% eventdata  reserved - to be defined in a future version of MATLAB
+% handles    empty - handles not created until after all CreateFcns called
+
+% Hint: edit controls usually have a white background on Windows.
+%       See ISPC and COMPUTER.
+if ispc && isequal(get(hObject,'BackgroundColor'), get(0,'defaultUicontrolBackgroundColor'))
+    set(hObject,'BackgroundColor','white');
+end
+
+
+% --- Executes on button press in pushbutton1.
+function pushbutton1_Callback(hObject, eventdata, handles)
+% hObject    handle to pushbutton1 (see GCBO)
+% eventdata  reserved - to be defined in a future version of MATLAB
+% handles    structure with handles and user data (see GUIDATA)
+
+out = Quantum_MAIN;
+
+plot(out.a, out.b, out.a, out.c)
+xlabel('Dimensionles Position')
+ylabel('Dimensionless PDF')
+legend('Analytical', 'Numerical')
+legend('boxoff')
+ax = gca;
+ax.FontSize = 20;
+
+
+% --- Executes on slider movement.
+function slider1_Callback(hObject, eventdata, handles)
+% hObject    handle to slider1 (see GCBO)
+% eventdata  reserved - to be defined in a future version of MATLAB
+% handles    structure with handles and user data (see GUIDATA)
+
+% Hints: get(hObject,'Value') returns position of slider
+%        get(hObject,'Min') and get(hObject,'Max') to determine range of slider
+maxNumberOfImages = 10;
+set(handles, 'Min', 1);
+set(handles, 'Max', maxNumberOfImages);
+set(handles, 'Value', 1);
+set(handles, 'SliderStep', [1/maxNumberOfImages , 10/maxNumberOfImages ]);
+
+% --- Executes during object creation, after setting all properties.
+function slider1_CreateFcn(hObject, eventdata, handles)
+% hObject    handle to slider1 (see GCBO)
+% eventdata  reserved - to be defined in a future version of MATLAB
+% handles    empty - handles not created until after all CreateFcns called
+
+% Hint: slider controls usually have a light gray background.
+if isequal(get(hObject,'BackgroundColor'), get(0,'defaultUicontrolBackgroundColor'))
+    set(hObject,'BackgroundColor',[.9 .9 .9]);
+end
+
+
+% --- Executes on slider movement.
+function slider2_Callback(hObject, eventdata, handles)
+% hObject    handle to slider2 (see GCBO)
+% eventdata  reserved - to be defined in a future version of MATLAB
+% handles    structure with handles and user data (see GUIDATA)
+
+% Hints: get(hObject,'Value') returns position of slider
+%        get(hObject,'Min') and get(hObject,'Max') to determine range of slider
+
+
+% --- Executes during object creation, after setting all properties.
+function slider2_CreateFcn(hObject, eventdata, handles)
+% hObject    handle to slider2 (see GCBO)
+% eventdata  reserved - to be defined in a future version of MATLAB
+% handles    empty - handles not created until after all CreateFcns called
+
+% Hint: slider controls usually have a light gray background.
+if isequal(get(hObject,'BackgroundColor'), get(0,'defaultUicontrolBackgroundColor'))
+    set(hObject,'BackgroundColor',[.9 .9 .9]);
+end
+
+
+% --- Executes on selection change in listbox1.
+function listbox1_Callback(hObject, eventdata, handles)
+% hObject    handle to listbox1 (see GCBO)
+% eventdata  reserved - to be defined in a future version of MATLAB
+% handles    structure with handles and user data (see GUIDATA)
+
+% Hints: contents = cellstr(get(hObject,'String')) returns listbox1 contents as cell array
+%        contents{get(hObject,'Value')} returns selected item from listbox1
+
+
+% --- Executes during object creation, after setting all properties.
+function listbox1_CreateFcn(hObject, eventdata, handles)
+% hObject    handle to listbox1 (see GCBO)
+% eventdata  reserved - to be defined in a future version of MATLAB
+% handles    empty - handles not created until after all CreateFcns called
+
+% Hint: listbox controls usually have a white background on Windows.
+%       See ISPC and COMPUTER.
+if ispc && isequal(get(hObject,'BackgroundColor'), get(0,'defaultUicontrolBackgroundColor'))
+    set(hObject,'BackgroundColor','white');
+end
+
+
+% --- Executes on slider movement.
+function slider3_Callback(hObject, eventdata, handles)
+% hObject    handle to slider3 (see GCBO)
+% eventdata  reserved - to be defined in a future version of MATLAB
+% handles    structure with handles and user data (see GUIDATA)
+
+% Hints: get(hObject,'Value') returns position of slider
+%        get(hObject,'Min') and get(hObject,'Max') to determine range of slider
+
+
+% --- Executes during object creation, after setting all properties.
+function slider3_CreateFcn(hObject, eventdata, handles)
+% hObject    handle to slider3 (see GCBO)
+% eventdata  reserved - to be defined in a future version of MATLAB
+% handles    empty - handles not created until after all CreateFcns called
+
+% Hint: slider controls usually have a light gray background.
+if isequal(get(hObject,'BackgroundColor'), get(0,'defaultUicontrolBackgroundColor'))
+    set(hObject,'BackgroundColor',[.9 .9 .9]);
+end
